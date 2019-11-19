f0e18c35d9335cdc025229c5fe303e281a07084e
unknown <phuocduc2012@gmail.com>
2019-11-19 15:01:57 +0700

    first commit

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request,redirect, url_for,flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin,login_user,current_user,login_required,logout_user
 from werkzeug.security import generate_password_hash, check_password_hash
-
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 
@@ -14,9 +14,13 @@ app.secret_key = "abc"
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 db = SQLAlchemy(app)
 
+migrate = Migrate(app, db)
+
+
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = "signin"
+
 
 class User(UserMixin,db.Model):
     __tablename__ = 'users'
@@ -26,7 +30,10 @@ class User(UserMixin,db.Model):
     email = db.Column(db.String(225), nullable= False, unique= True)
     password = db.Column(db.String(225), nullable= False)
     img_url = db.Column(db.Text)
-   
+
+    post = db.relationship('Post',backref="user",lazy=True)
+
+    like_post = db.relationship('Post', secondary="likes", backref="my_likes", lazy=True)
     def set_password(self, password):
         self.password = generate_password_hash(password)
     def check_password(self,password):
@@ -34,14 +41,23 @@ class User(UserMixin,db.Model):
 
 db.create_all()
 
-class Blog(db.Model):
+class Post(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     author=db.Column(db.String(255), nullable=False)
-    user_id = db.Column(db.Integer, nullable= False)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
     body = db.Column(db.Text, nullable = False)
     created = db.Column(db.DateTime, server_default=db.func.now())
     updated = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now()),
     img_url = db.Column(db.Text)
+    view_count = db.Column(db.Integer, default = 0)
+
+likes = db.Table('likes',
+            db.Column('user_id', db.Integer, db.ForeignKey('users.id'), primary_key=True),
+            db.Column('post_id', db.Integer, db.ForeignKey('post.id'), primary_key=True))
+
+
 
 db.create_all()
 
@@ -50,7 +66,7 @@ class Comment(db.Model):
     body = db.Column(db.String, nullable = False)
     author_comment= db.Column(db.String, nullable= False)
     user_id = db.Column(db.Integer, nullable = False)
-    comment_id = db.Column(db.Integer, nullable=False)
+    post_id = db.Column(db.Integer, nullable=False)
     created = db.Column(db.DateTime, server_default=db.func.now())
     updated = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
     img_url = db.Column(db.Text)
@@ -64,9 +80,6 @@ db.create_all()
 def load_user(user_id):
     return User.query.get(user_id)
 
-@app.route('/index',methods=['GET'])
-def root():
-    return render_template("view/body.html")
 
 
 
@@ -84,92 +97,96 @@ def signup():
         db.session.commit()
         flash("You have signed up successfully", 'success')
         return redirect(url_for("signup"))
-    return render_template("signup.html")
+    return render_template("view/signup.html")
 
 
 @app.route('/', methods=['GET','POST'])
 def signin():
     if current_user.is_authenticated:
-        return redirect(url_for('blog'))
+        return redirect(url_for('main_page'))
     if request.method == "POST":
         user = User.query.filter_by(email=request.form['email']).first()
         if user:
             if user.check_password(request.form['password']):
                 login_user(user)
                 flash('welcome {0}'.format(user.email), 'success')
-                return redirect(url_for("blog"))
+                return redirect(url_for("main_page"))
             flash('Incorrect Email or Password','warning')
             return redirect(url_for("signin"))
         flash('Incorrect Email or Password','warning')
         return redirect(url_for("signin"))
-    return render_template("signin.html")
+    return render_template("view/signin.html")
+
+
+
+@app.route('/blog', methods=['GET','POST'])
+@login_required
+def main_page():
+    if not current_user.is_authenticated:
+        return redirect(url_for("signin"))
+    if request.method == "POST":
+        new_post = Post(body = request.form['body'],author=current_user.email.split("@")[0],
+        img_url = current_user.img_url)
+        current_user.post.append(new_post)
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect(url_for("main_page"))
+    posts = Post.query.all()
+    for post in posts:
+        post.comments = Comment.query.filter_by(post_id= post.id).first()
+    return render_template('view/blog.html', posts = posts)
+
+
+
+#delete an entry
+@app.route('/blog/<id>', methods = ['GET','POST'])
+def delete_blog(id):
+    if request.method == "POST":
+        post = Post.query.filter_by(id = id).first()
+        if not post:
+            return "there is no such post"
+        db.session.delete(post)
+        db.session.commit()
+        return redirect(url_for("main_page"))
+    return "Not allowed"
+
+
+
+@app.route('/post/<id>', methods=['GET','POST'])
+@login_required
+def view_post(id):
+    post = Post.query.get(id)
+    post.view_count +=1
+    db.session.commit()
+    if request.method == "POST":
+        new_comment = Comment(body=request.form['body'],user_id=current_user.id,post_id=id, author_comment=current_user.email.split("@")[0], img_url=current_user.img_url)
+        db.session.add(new_comment)
+        db.session.commit()
+        return redirect(url_for("view_post", id=id))
+    comments = Comment.query.filter_by(post_id=id).all()
+    for comment in comments:
+        comment.author_comment = User.query.filter_by(id = comment.user_id).first().email
+    return render_template("view/post.html", post=post, comments = comments)
+
+@app.route('/like/post/<int:id>', methods=['POST'])
+@login_required
+def like_post(id):
+    post = Post.query.get(id)
+    if not post.my_likes:
+        current_user.like_post.append(post)
+        db.session.commit()
+        return redirect(url_for("main_page"))
+    current_user.like_post.remove(post)
+    db.session.commit()
+    return redirect(url_for("main_page"))
+
 
 
 @app.route('/logout')
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for("root"))
-
-# @app.route("/post_new", methods=['GET','POST'])
-# @login_required
-# def post_new():
-#     if not current_user.is_authenticated:
-#         return redirect(url_for("singin"))
-#     if request.method == "POST":
-
-@app.route('/blog', methods=['GET','POST'])
-@login_required
-#add new entry
-def blog():
-    if not current_user.is_authenticated:
-        return redirect(url_for("signin"))
-    if request.method == "POST":
-        # print('author',authors)
-        new_blog = Blog(body = request.form['body'],user_id=current_user.id,author=current_user.email.split("@")[0],
-        img_url = current_user.img_url)
-
-        db.session.add(new_blog)
-        db.session.commit()
-        return redirect(url_for("blog"))
-    posts = Blog.query.all()
-    for post in posts:
-        post.comments = Comment.query.filter_by(comment_id= post.id).all()
-    return render_template('blog.html', posts = posts)
-
-#delete an entry
-@app.route('/blog/<id>', methods = ['GET','POST'])
-def delete_blog(id):
-    if request.method == "POST":
-        
-        post = Blog.query.filter_by(id = id).first()
-        if not post:
-            return "there is no such post"
-        db.session.delete(post)
-        db.session.commit()
-        return redirect(url_for("blog"))
-    return "Not allowed"
-
-@app.route('/blog/<id>', methods = ['GET', 'POST'])
-def update_blog(id):
-    if request.method == "POST":
-        new_blog = Blog(body = request.form['comment'],user_id=current_user.id)
-        db.session.add(new_blog)
-        db.session.commit()
-
-
-@app.route('/blog/<id>/comment/', methods=['POST'])
-def comment_post(id):
-    if request.method == "POST":
-        new_comment = Comment(body=request.form['comment'],user_id=current_user.id,comment_id=id, author_comment=current_user.email.split("@")[0], img_url=current_user.img_url)
-        db.session.add(new_comment)
-        db.session.commit()
-        return redirect(url_for("blog"))
-    comments = Comment.query.all()
-    for comment in comments:
-        comment.author_comment = Comment.query.filter_by(comment_id = comment.id).all()
-    return render_template("blog.html", comments = comments )
-
+    return redirect(url_for("signin"))
 
 
 
